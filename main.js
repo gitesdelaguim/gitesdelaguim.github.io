5957fae18a1b601a3a612acd2d3962c7f018f0be
Romain GOUPIL <romain.goupil@netc.fr>
2022-02-27 15:18:07 +0100

    feat: first vuetify website with all the pages

---
+++ main.js
@@ -0,0 +1,14 @@
+new Vue({
+    el: '#app',
+   vuetify: new Vuetify(),
+   data: {
+       landscapes: [
+           {
+               src: './res/landscape/Meinga.jpg',
+           },
+           {
+               src: './res/landscape/Meinga.jpg',
+           }
+       ],
+   },
+})
